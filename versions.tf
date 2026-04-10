3f6d3d8d7d5f6bea7b14a0fdbe5a015dd4ac43ce
Stas Davydov <davidovsv@yandex.ru>
2026-04-10 23:59:17 +0400

    Updated aws provider versions

--- versions.tf
+++ versions.tf
@@ -10,7 +10,7 @@ terraform {
 
   provider_meta "aws" {
     user_agent = [
-      "github.com/terraform-aws-modules/terraform-aws-ecs"
+      "github.com/stasdavydov/terraform-aws-ecs"
     ]
   }
 }
