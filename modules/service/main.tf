3a49f086952ca01c1d5951275793fb1943304cc8
Stas Davydov <davidovsv@yandex.ru>
2026-04-16 00:46:27 +0400

    Aligned versions

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -1060,9 +1060,9 @@ resource "aws_ecs_task_definition" "this" {
         for_each = volume.value.s3files_volume_configuration != null ? [volume.value.s3files_volume_configuration] : []
 
         content {
-          file_system_arn         = s3files_volume_configuration.value.file_system_arn
-          root_directory          = s3files_volume_configuration.value.root_directory
-          access_point_arn        = s3files_volume_configuration.value.access_point_arn
+          file_system_arn  = s3files_volume_configuration.value.file_system_arn
+          root_directory   = s3files_volume_configuration.value.root_directory
+          access_point_arn = s3files_volume_configuration.value.access_point_arn
         }
       }
 
